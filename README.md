b63d00b62e81f6a201d1ec2bac75c6ad3c6836fc
CNOCTAVE <cnoctave@qq.com>
2025-06-19 17:48:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# rolling-subtitle
+ rolling-subtitle - play rolling subtitle in browser.
+# rolling-subtitle Document
+ Check out document: [rolling-subtitle Document](https://cnoctave.github.io/rolling-subtitle/index.html)
+# How to translate rolling-subtitle Document into another language
+ In ./docs directory, index.html is zh-CN simplified Chinese document. 
+ For example, if you want to translate document into English.
+ 1. Copy index.html as another document with different language code as filename, 
+ for example, en-US.html.
+ 2. Translate en-US.html into English.
+ 3. Add dropdown like the picture below to every *.html. 
+ For example, add dropdown "en-US English".  
+ ![the dropdown looking](./docs/translate_dropdown.png)  
+ The code for adding dropdown is like the picture below.  
+ ![the dropdown code](./docs/translate_dropdown_code.png) 
+ 4. PR to rolling-subtitle.
---
+++ rolling-subtitle.js
@@ -0,0 +1,135 @@
+// rolling-subtitle - play rolling subtitle in browser.
+// Copyright (C) 2024-2025  Yu Hongbo, CNOCTAVE
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+class SubtitleInstance {
+    constructor(element, container) {
+        this.element = element;
+        this.container = container;
+        this.animationId = null;
+    }
+
+    /**
+     * 开始滚动字幕
+     * @param {number} speed - 滚动速度(像素/帧)
+     */
+    play(speed = 1) {
+        // 重置位置
+        this.element.style.left = this.container.offsetWidth + 'px';
+        
+        // 停止之前的动画
+        if (this.animationId) {
+            cancelAnimationFrame(this.animationId);
+        }
+        
+        // 动画循环
+        const animate = () => {
+            const currentLeft = parseInt(this.element.style.left);
+            const newLeft = currentLeft - speed;
+            
+            // 如果完全滚出左侧，则重置到右侧
+            if (newLeft < -this.element.offsetWidth) {
+                this.element.style.left = this.container.offsetWidth + 'px';
+            } else {
+                this.element.style.left = newLeft + 'px';
+            }
+            
+            this.animationId = requestAnimationFrame(animate);
+        };
+        
+        animate();
+    }
+    
+    /**
+     * 停止滚动
+     */
+    stop() {
+        if (this.animationId) {
+            cancelAnimationFrame(this.animationId);
+            this.animationId = null;
+        }
+    }
+    
+    /**
+     * 获取字幕容器元素
+     */
+    getElement() {
+        return this.container;
+    }
+}
+
+class RollingSubtitle {
+    /**
+     * 初始化字幕元素
+     * @param {string} text - 要显示的字幕文本
+     * @param {object} options - 配置选项
+     * @param {string|HTMLElement} parent - 父元素ID或DOM元素
+     * @returns {SubtitleInstance} 字幕实例对象
+     */
+    static init(text, options = {}, parent) {
+        // 创建外层容器
+        const container = document.createElement('div');
+        container.className = 'rolling-subtitle-container';
+        
+        // 创建字幕元素
+        const subtitle = document.createElement('div');
+        subtitle.className = 'rolling-subtitle';
+        subtitle.textContent = text;
+        
+        // 应用样式
+        container.style.cssText = [
+            'position: relative',
+            'overflow: hidden',
+            'white-space: nowrap',
+            `width: ${options.width || '100%'}`,
+            `height: ${options.height || '50px'}`,
+            `line-height: ${options.height || '50px'}`,
+            `background-color: ${options.bgColor || 'transparent'}`,
+            `color: ${options.color || '#000'}`,
+            `font-size: ${options.fontSize || '16px'}`
+        ].join(';');
+        
+        subtitle.style.cssText = [
+            'position: absolute',
+            'display: inline-block'
+        ].join(';');
+        
+        container.appendChild(subtitle);
+        
+        // 自动添加到父元素
+        if (parent) {
+            const parentEl = typeof parent === 'string' 
+                ? document.getElementById(parent) 
+                : parent;
+            if (parentEl) {
+                parentEl.appendChild(container);
+                if (options.top === true) {
+                    parentEl.classList.add('top-subtitle');
+                }
+                else if (options.bottom === true) {
+                    parentEl.classList.add('bottom-subtitle');
+                }
+            }
+        }
+        
+        return new SubtitleInstance(subtitle, container);
+    }
+}
+
+// UMD导出
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.RollingSubtitle = factory());
+}(this, () => RollingSubtitle));
